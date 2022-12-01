cab85adb073e95dcff8c3de82f068d079dee4030
Saurav-rajk <118835139+Saurav-rajk@users.noreply.github.com>
2022-12-01 10:23:06 +0530

    Add files via upload

---
+++ class18-variable in python-CipherSchools.py
@@ -0,0 +1,16 @@
+number1=2
+print(numberl)
+number1 = 4
+print(number1)
+string, number
+name = "Saurav"
+print(name)
+name= 123
+print(name)
+#rule one
+#1number=4
+#letter ..................... first_letter
+_name= "Saurav"
+#ulsd = 34
+user_one_name = "Saurav" # snake case writing
+userOneName = "Saurav" # camel case writing
---
+++ class30-step argument-CipherSchools.py
@@ -0,0 +1,2 @@
+# print("Saurav"[-1::-1])
+print("Saurav"[::-1]) # trick
